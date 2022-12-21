70c145f7719aa702c4903c18f03b11a969723fb4
Donatien <donatien-rouzeirol@lapiscine.pro>
2022-12-21 16:57:41 +0100

    issues ingredient route

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -6,12 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class IngredientController extends AbstractController
 {
-    #[Route('/', 'home.index')]
+    #[Route('/ingredient', name: 'ingredient')]
     public function index(): Response
-
     {
-        return $this->render('pages/home.html.twig');
+        return $this->render('pages/ingredient/index.html.twig', [
+            'controller_name' => 'IngredientController',
+        ]);
     }
 }
